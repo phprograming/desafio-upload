c2a34e18406918af740f01066f1032f6e3f8db95
Paulo Henrique Barbosa Santos <paulo_hbs17@hotmail.com>
2021-03-17 01:43:39 -0300

    Commit correct

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,28 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    const { income, outcome } = transactions.reduce(
+      (add: Balance, transaction: Transaction) => {
+        if (transaction.type === 'income') {
+          add.income += Number(transaction.value);
+        } else {
+          add.outcome += Number(transaction.value);
+        }
+
+        return add;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 }
 
